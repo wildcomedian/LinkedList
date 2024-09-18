4682fe5b5bb5ca83bb5eed452796ffb1a25b1fcc
menchukf <77518360+menchukf@users.noreply.github.com>
2024-09-18 09:23:24 -0400

    Create LinkedList.java

---
+++ workspace/LinkedList.java
@@ -0,0 +1,52 @@
+/*
+Problem:  Write a program that keeps and manipulates a linked list of
+	    String data. The data will be provided by the user one item at a time.
+      The user should be able to do the following operations:
+                     -add "String"
+                                adds an item to your list (maintaining alphabetical order)
+                     -remove "String"
+                                if the item exists removes the first instance of it
+                     -show
+                                should display all items in the linked list
+                     -clear
+                               should clear the list
+	Input:  commands listed above
+	Output:  the results to the screen of each menu
+	    choice, and error messages where appropriate.
+*/
+public class LinkedList{
+
+  //instance varialbes go here (think about what you need to keep track of!)
+
+  //constructors go here
+
+
+  //precondition: the list has been initialized
+  //postcondition: the ListNode containing the appropriate value has been added and returned
+  public ListNode addAValue(String line)
+  {
+    return null;
+  }
+
+  //precondition: the list has been initialized
+  //postcondition: the ListNode containing the appropriate value has been deleted and returned.
+  //if the value is not in the list returns null
+  public ListNode deleteAValue(String line)
+  {
+    return null;
+  }
+
+  //precondition: the list has been initialized
+  //postconditions: returns a string containing all values appended together with spaces between.
+  public String showValues()
+  {
+    return null;
+  }
+
+  //precondition: the list has been initialized
+  //postconditions: clears the list.
+  public void clear()
+  {
+  
+  }
+}
